9dbb22fc7916b6990cfe02910390b761e8bea015
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-10-17 22:27:55 +0200

    feat: Page migration

---
+++ app/Models/Page.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableObserver;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Stevebauman\Purify\Casts\PurifyHtmlOnGet;
+
+class Page extends Model
+{
+    use HasFactory, Sluggable;
+
+    protected $fillable = ['title', 'slug', 'content'];
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => ['title', 'id'],
+            ],
+        ];
+    }
+
+    /**
+     * Slug generation event.
+     *
+     * @return string
+     */
+    public function sluggableEvent(): string
+    {
+        /**
+         * Optional behaviour -- generate slug after model is saved.
+         * This will likely become the new default in the next major release.
+         */
+        return SluggableObserver::SAVED;
+    }
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+            'content' => PurifyHtmlOnGet::class, // Purify HTML content on get
+        ];
+    }
+}
