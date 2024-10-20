873f3a2f38e1bee18d0629976ca3aba6629ea571
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-10-20 20:29:02 +0200

    feat: admin blog comment show

---
+++ app/Http/Controllers/Admin/AdminCommentBlogController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use App\Models\CommentBlog;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class AdminCommentBlogController extends Controller
+{
+    public function index(string $id, Request $request): \Inertia\Response
+    {
+        $comments = CommentBlog::query();
+        $comments->where('blog_id', $id);
+
+        if (!$request->has('created_at')) {
+            $comments->orderBy('created_at', 'desc');
+        }
+
+        if ($request->has('search') && $request->search !== null) {
+            $comments->where('content', 'like', '%' . $request->search . '%')->orWhereHas('user', function ($query) use ($request) {
+                $query->where('name', 'like', '%' . $request->search . '%');
+            });
+        }
+
+        if ($request->has('created_at') && $request->created_at !== null) {
+            $comments->orderBy('created_at', $request->created_at === 'desc' ? 'desc' : 'asc');
+        }
+
+        $comments = $comments->paginate($request->per_page ?? 10);
+
+        return Inertia::render('Admin/Blog/Comment/List', [
+            'blog' => Blog::where('id', $id)->first(),
+            'comments' => $comments->getCollection()->map(function ($comment) {
+                return [
+                    'id' => $comment->id,
+                    'content' => $comment->content,
+                    'created_at' => $comment->created_at,
+                    'user' => [
+                        'name' => $comment->user->name,
+                        'id' => $comment->user->id,
+                    ],
+                ];
+            }),
+            'pagination' => [
+                'current_page' => $comments->currentPage(),
+                'last_page' => $comments->lastPage(),
+                'per_page' => $comments->perPage(),
+                'total' => $comments->total(),
+            ],
+            'filters' => $request->all(),
+        ]);
+    }
+
+    public function show(string $id): \Inertia\Response
+    {
+        $comment = CommentBlog::where('id', $id)->with('user')->firstOrFail();
+
+        return Inertia::render('Admin/Blog/Comment/Show', [
+            'comment' => [
+                'id' => $comment->id,
+                'content' => $comment->content,
+                'created_at' => $comment->created_at,
+                'user' => [
+                    'name' => $comment->user->name,
+                    'id' => $comment->user->id,
+                ],
+            ],
+            'blog' => Blog::where('id', $comment->blog_id)->first(),
+        ]);
+    }
+}
