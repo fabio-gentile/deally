ef25c6b51ff5fe0ea127af0d46456fd69b9faab0
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-10-22 15:10:29 +0200

    feat: home pagination and filters

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,11 +6,7 @@ use App\Models\Blog;
 use App\Models\CategoryDeal;
 use App\Models\Deal;
 use App\Models\Discussion;
-use App\Models\VoteDeals;
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 class HomeController extends Controller
@@ -21,33 +17,62 @@ class HomeController extends Controller
     public function index(Request $request): \Inertia\Response
     {
         $user = auth()->user();
-//        $user->assignRole('admin');
-        $deals = Deal::with(['voteDetails' => function ($query) use ($user) {
-            // filter the vote of the user
+        $deals = Deal::query();
+
+        // by default, only show deals created in the last 30 days
+        if (!$request->filled('period')) {
+            $deals->where('created_at', '>', now()->subDays(30));
+        }
+
+        if ($request->filled('period')) {
+            $period = $request->input('period');
+            $date = new \DateTime();
+            if ($period === 'day') {
+                $deals->where('created_at', '>=', $date->modify('-1 day'));
+            } elseif ($period === 'week') {
+                $deals->where('created_at', '>=', $date->modify('-1 week'));
+            } elseif ($period === 'month') {
+                $deals->where('created_at', '>=', $date->modify('-1 month'));
+            }
+        }
+
+        $deals->with(['voteDetails' => function ($query) use ($user) {
             if ($user) {
                 $query->where('user_id', $user->id);
-            };
+            }
         }, 'images' => function ($query) {
+            // Limit the number of images to 1
             $query->limit(1);
         }, 'favorites' => function ($query) use ($user) {
             if ($user) {
                 $query->where('user_id', $user->id);
             }
-        }])->limit(10)->get();
+        }
+        ]);
 
-        // add the user vote to the deal
+        $deals->withCount('comments');
+        $deals = $deals->paginate(10);
+
         if ($user) {
             $deals->each(function ($deal) use ($user) {
-                // associate the voteDeals to the deal
                 $deal->user_vote = $deal->voteDetails->first();
                 $deal->is_expired = $deal->isExpired();
                 $deal->user_favorite = $deal->favorites->isNotEmpty();
             });
         }
-//dd($deals);
+
+        $pagination = [
+            'current_page' => $deals->currentPage(),
+            'last_page' => $deals->lastPage(),
+            'per_page' => $deals->perPage(),
+            'total' => $deals->total(),
+        ];
+
         return Inertia::render('Home', [
             'categories' => CategoryDeal::all(),
-            'deals' => $deals,
+            'deals' => $deals->items(),
+            'filters' => $request->all(),
+            'pagination' => $pagination,
             'discussions' => Discussion::orderBy('created_at', 'desc')->with('user')->withCount('comments')->limit(5)->get(),
             'blogs' => Blog::orderBy('published_at', 'desc')->where('is_published', true)->withCount('comments')->limit(5)->get(),
         ]);
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Head, Link } from "@inertiajs/vue3"
+import { Head, Link, router } from "@inertiajs/vue3"
 import Button from "@/Components/ui/button/Button.vue"
 import { CategoryDeal } from "@/types/model/deal"
 import Wrapper from "@/Components/layout/Wrapper.vue"
@@ -16,13 +16,67 @@ import HomeDiscussion from "@/Components/Home/HomeDiscussion.vue"
 import { Discussion } from "@/types/model/discussion"
 import { Blog } from "@/types/model/blog"
 import HomeBlog from "@/Components/Home/HomeBlog.vue"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/Components/ui/select"
+import { Pagination as IPagination } from "@/types/model/miscellaneous"
+import { ref, watch } from "vue"
+import {
+  Pagination,
+  PaginationEllipsis,
+  PaginationList,
+  PaginationListItem,
+  PaginationNext,
+  PaginationPrev,
+} from "@/Components/ui/pagination"
+import { Frown } from "lucide-vue-next"
 
-defineProps<{
+interface Filters {
+  page: number
+  period?: "today" | "week" | "month"
+}
+
+const props = defineProps<{
   categories: CategoryDeal[]
   deals: Deal[]
   discussions: Discussion[]
   blogs: Blog[]
+  pagination: IPagination
+  filters: Filters
 }>()
+
+const filters = ref({ ...props.filters })
+const deals = ref(props.deals)
+
+const search = () => {
+  router.get(route("home.index"), filters.value, {
+    preserveState: true,
+    replace: true,
+    onSuccess: () => {
+      deals.value = props.deals
+    },
+  })
+}
+
+watch(
+  filters,
+  () => {
+    search()
+  },
+  { deep: true }
+)
+
+// Function to handle page change
+const changePage = (page: number) => {
+  filters.value.page = page
+  search()
+}
 </script>
 
 <template>
@@ -45,7 +99,6 @@ defineProps<{
                 :key="category.slug"
                 class="flex basis-auto !pl-0"
               >
-                <!--                TODO: add route redirection-->
                 <Link
                   :href="route('search.deals') + '?category=' + category.name"
                 >
@@ -85,13 +138,90 @@ defineProps<{
 
     <main class="bg-page">
       <Wrapper class="pt-8">
+        <div class="mb-4 flex flex-row items-center gap-4">
+          <p class="font-medium text-muted-foreground">
+            Deals les plus populaires les
+          </p>
+          <Select
+            v-model="filters.period"
+            @update:model-value="filters.page = 1"
+          >
+            <SelectTrigger class="!w-fit gap-2">
+              <SelectValue placeholder="30 derniers jours" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel class="sr-only"
+                  >Deals les plus populaires</SelectLabel
+                >
+                <SelectItem value="day"> dernières 24 heures </SelectItem>
+                <SelectItem value="week"> 7 derniers jours </SelectItem>
+                <SelectItem value="month"> 30 derniers jours </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
         <div class="flex flex-col gap-3 lg:flex-row lg:gap-6">
           <div class="flex grow flex-col gap-3">
+            <div
+              v-if="deals.length < 1"
+              class="flex flex-col items-center justify-center gap-6"
+            >
+              <Frown class="h-32 w-32 text-muted-foreground" />
+              <div>
+                <p class="text-center text-lg font-semibold">
+                  Aucun bon plan trouvé
+                </p>
+                <p class="text-muted-foreground">
+                  Essayez de changer la période.
+                </p>
+              </div>
+            </div>
             <CardDeal
               v-for="(deal, index) in deals"
               :key="index"
               :deal="deal"
             />
+            <Pagination
+              class="mt-4"
+              v-slot="{ page }"
+              :total="pagination.total"
+              :sibling-count="1"
+              :default-page="1"
+              show-edges
+            >
+              <PaginationList
+                v-if="pagination.total > 0"
+                v-slot="{ items }"
+                class="flex items-center justify-center gap-1"
+              >
+                <PaginationPrev
+                  @click="changePage(pagination.current_page - 1)"
+                />
+
+                <template v-for="(item, index) in items">
+                  <PaginationListItem
+                    v-if="item.type === 'page'"
+                    :key="index"
+                    :value="item.value"
+                    as-child
+                  >
+                    <Button
+                      class="h-10 w-10 p-0"
+                      @click="changePage(item.value)"
+                      :variant="item.value === page ? 'default' : 'outline'"
+                    >
+                      {{ item.value }}
+                    </Button>
+                  </PaginationListItem>
+                  <PaginationEllipsis v-else :key="item.type" :index="index" />
+                </template>
+
+                <PaginationNext
+                  @click="changePage(pagination.current_page + 1)"
+                />
+              </PaginationList>
+            </Pagination>
           </div>
           <div
             class="flex w-full shrink-0 flex-col justify-between gap-4 md:flex-row lg:w-fit lg:flex-col lg:justify-normal"
